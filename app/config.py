aadb79dae9f43ac68e7c1f992938d390a29b3780
Apple <apple@Abduls-MacBook-Pro.local>
2024-09-10 21:40:58 +0500

    fix config & ui

--- app/config.py
+++ app/config.py
@@ -3,7 +3,8 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY") or "supersekrit"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.sqlite3"
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///app.sqlite3"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:////home/abdul/LetScan/letscan/instance/app.sqlite3"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     GOOGLE_OAUTH_CLIENT_ID = '726773540164-feratonqe02cnho2in1nn451kub737as.apps.googleusercontent.com'
     GOOGLE_OAUTH_CLIENT_SECRET = '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'
