9664de3b46cc1a138158547b5abeba62b565c89d
Abdul Rehman <abdul12391@gmail.com>
2024-09-11 18:03:12 +0500

    update config

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get("FLASK_SECRET_KEY", "default_secret_key")
-
+    SESSION_COOKIE_SECURE = True
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///instance/app.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
